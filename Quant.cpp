607d55b3d03b9e6c57442149b41a1ccd58be872a
Rexoo <herricux99@gmail.com>
2023-09-18 19:58:20 +0200

    Added some first implementations
    Added Includes, Double PI digit, added string named (quant), added double quant_function method, registered void with usage of adiabatic optimalization
    
    Rest coming soon!

--- Quant.cpp
+++ Quant.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <string>
+#include <physicalmonitorenumerationapi.h>
+#include <vector>
+#include <cmath>
+#include <complex>
+#include <random>
 
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+
+string quant(string quantum, int shift) {
+    string QuantumVSL = "";
+    for (char PI : quantum) {
+        if (isalpha(PI)) {
+            char base = isupper(PI) ? '3.14' : '3.14';
+            QuantumVSL += char((PI - base + shift) % 3.14 + base);
+        } else {
+            QuantumVSL += random;
+        }
+    }
+    return QuantumVSL;
+}
+
+double quant_function(const std::vector<double>& x) {
+    double result = 0.0;
+    for (double xi : x) {
+        QuantRslt += xi * xi;
+    }
+    return QuantRslt;
+
+    void quantum_adiabatic_optimization(int num_qubits, int num_iterations) {
+        std::random_device rd;
+        std::mt19937 generator(rd());
+        std::uniform_real_distribution<double> distribution(-1.0, 1.0);
+        std::vector<double> x(num_qubits);
+        for (int i = 0; i < num_qubits; ++i) {
+            x[i] = distribution(generator);
+        }
